a1a22c0832476422ba8a6b92f523a2e87341ff88
Aditya <aditya311001rj@gmail.com>
2026-03-25 14:51:57 +0530

    update WalletConnect project ID

--- src/walletConfig.jsx
+++ src/walletConfig.jsx
@@ -3,6 +3,6 @@ import { mainnet, base, arbitrum, optimism } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "Yieldo Wallets",
-  projectId: "b1e8d43be4da18ec27f9ee18c2b6e29a",
+  projectId: "0dd252f3816efa3917348bf2b60af0aa",
   chains: [mainnet, base, arbitrum, optimism],
 });
